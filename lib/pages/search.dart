53f818edf6104d635a3f7e2b7c074ca0a8b25bfa
CharlesMuchogo <muchpaul2@gmail.com>
2022-07-16 17:24:52 +0300

    Search Improved

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:matibabu/GlobalComponents/restapi.dart';
 import 'package:matibabu/pages/doctor_information.dart';
 
 class Search extends StatefulWidget {
@@ -11,24 +12,16 @@ class Search extends StatefulWidget {
   State<Search> createState() => _SearchState();
 }
 
+TextEditingController searchdoctorcontroler = TextEditingController();
+
 class _SearchState extends State<Search> {
-  TextEditingController searchdoctorcontroler = TextEditingController();
-
-  void _printLatestValue() {
-    print("$searchdoctorcontroler.text");
-  }
-
   @override
   void initState() {
     super.initState();
-
-    searchdoctorcontroler.addListener(_printLatestValue);
   }
 
-  @override
-  void dispose() {
-    searchdoctorcontroler.dispose();
-    super.dispose();
+  onSearch(String search) {
+    setState(() {});
   }
 
   @override
@@ -44,51 +37,39 @@ class _SearchState extends State<Search> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Container(
-                    height: 39,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(30),
-                      ),
-                      color: Color.fromRGBO(245, 242, 242, 10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(0.0, 1.0), //(x,y)
-                          blurRadius: 6.0,
-                        ),
-                      ],
+            Center(
+              child: Container(
+                height: 39,
+                width: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30),
+                  ),
+                  color: Color.fromRGBO(245, 242, 242, 10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 1.0), //(x,y)
+                      blurRadius: 6.0,
                     ),
-                    child: Container(
-                      width: 300,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 30, right: 30),
-                        child: TextField(
-                          controller: searchdoctorcontroler,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              //labelText: 'Enter Name',
-                              hintText: 'Search for a doctor'),
-                        ),
-                      ),
+                  ],
+                ),
+                child: Container(
+                  width: 300,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 30),
+                    child: TextField(
+                      onChanged: (val) => onSearch(val),
+                      controller: searchdoctorcontroler,
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          border: InputBorder.none,
+                          //labelText: 'Enter Name',
+                          hintText: 'Search for a doctor'),
                     ),
                   ),
                 ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      searchdoctorcontroler.text =
-                          searchdoctorcontroler.text.trim();
-                    });
-                  },
-                  icon: Icon(Icons.search),
-                )
-              ],
+              ),
             ),
             searchinfo(searchdoctorcontroler.text)
           ],
@@ -125,7 +106,7 @@ Widget searchinfo(String controler) {
                 ...(snapshot.data!.docs
                     .where(
                   (QueryDocumentSnapshot<Object?> element) =>
-                      element["specialty"]
+                      element["First Name"]
                           .toString()
                           .toLowerCase()
                           .contains(controler.toLowerCase()),
