9c5404ae5ecbfa7a52b9f7f5a84139007ca86608
CharlesMuchogo <muchpaul2@gmail.com>
2022-07-19 12:29:42 +0300

    Fixed Signup

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -2,48 +2,198 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:matibabu/pages/login.dart';
-import 'package:matibabu/pages/signupfields.dart';
+import 'package:matibabu/GlobalComponents/authenticationservice.dart';
 
-class Signup extends StatelessWidget {
+import 'package:matibabu/GlobalComponents/restapi.dart';
+import 'package:matibabu/pages/login.dart';
+import 'package:provider/provider.dart';
+
+class Signup extends StatefulWidget {
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  TextEditingController firstNamecontroler = TextEditingController();
+  TextEditingController lastNamecontroler = TextEditingController();
+  TextEditingController emailcontroler = TextEditingController();
+  TextEditingController phoneNumbercontroler = TextEditingController();
+  TextEditingController passwordcontroler = TextEditingController();
+  TextEditingController confirmpasswordcontroler = TextEditingController();
+
+  TextEditingController addresscontroler = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     double heightOfDevice = MediaQuery.of(context).size.height;
+    bool _isLoading = false;
+
+    void _input() async {
+      if ((firstNamecontroler.text.trim().isEmpty ||
+          lastNamecontroler.text.trim().isEmpty ||
+          emailcontroler.text.trim().isEmpty ||
+          phoneNumbercontroler.text.trim().isEmpty ||
+          passwordcontroler.text.trim().isEmpty ||
+          confirmpasswordcontroler.text.trim().isEmpty ||
+          addresscontroler.text.trim().isEmpty)) {
+        return showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Error !"),
+                content: Text("Fill in all the details"),
+                actions: [
+                  MaterialButton(
+                      child: Text("Okay"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      })
+                ],
+              );
+            });
+      } else {
+        if (passwordcontroler.text != confirmpasswordcontroler.text) {
+          return showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Error !"),
+                  content: Text("Passwords do not match"),
+                  actions: [
+                    MaterialButton(
+                        child: Text("Okay"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        })
+                  ],
+                );
+              });
+        }
+        await context.read<AuthenticationService>().signUp(
+            context: context,
+            email: emailcontroler.text.toLowerCase().trim(),
+            password: passwordcontroler.text.trim());
+
+        RestApi rest = RestApi();
+
+        await rest.createData(
+          context,
+          firstNamecontroler.text.trim(),
+          lastNamecontroler.text.trim(),
+          emailcontroler.text.trim(),
+          phoneNumbercontroler.text,
+          addresscontroler.text.trim(),
+        );
+      }
+
+      return Navigator.of(context).pop();
+    }
 
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: heightOfDevice * 0.25,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: FutureBuilder<Object>(
+          future: null,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              setState(() {
+                _isLoading = true;
+              });
+              return MaterialApp(
+                home: Scaffold(
+                  body: Center(child: CircularProgressIndicator()),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return MaterialApp(
+                home: Scaffold(
+                  body: Center(child: Text("Error Occured")),
+                ),
+              );
+            }
+
+            return Column(
               children: [
-                logo("Matibabu", 32, Colors.teal),
-                logo("Health Is Wealth", 15, Colors.black)
+                SizedBox(
+                  height: heightOfDevice * 0.25,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      logo("Matibabu", 32, Colors.teal),
+                      logo("Health Is Wealth", 15, Colors.black)
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Container(
+                    height: heightOfDevice * 0.75,
+                    padding: EdgeInsets.all(20),
+                    child: ListView(
+                      children: [
+                        textfields(firstNamecontroler, "Enter your first name",
+                            TextInputType.name),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        textfields(lastNamecontroler, "Enter your last name",
+                            TextInputType.name),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        textfields(emailcontroler, "Enter your email",
+                            TextInputType.emailAddress),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        textfields(phoneNumbercontroler, "Enter phone number",
+                            TextInputType.phone),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        textfields(addresscontroler, "Enter your address",
+                            TextInputType.name),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        passwordFields(passwordcontroler, "Create password"),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        passwordFields(
+                            confirmpasswordcontroler, "Confirm password"),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          height: 42,
+                          width: 196,
+                          child: _isLoading
+                              ? CircularProgressIndicator()
+                              : ElevatedButton(
+                                  onPressed: _input,
+                                  child: Text("Sign up"),
+                                  style: ElevatedButton.styleFrom(
+                                      primary:
+                                          Color.fromRGBO(43, 147, 128, 20)),
+                                ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ],
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Container(
-                    padding: EdgeInsets.all(20), child: SignupFields()),
-              ),
-            ],
-          ),
-        ],
-      ),
-    ));
+            );
+          }),
+    );
   }
 }
 
 Widget textfields(TextEditingController textfieldcontroler, String placeholder,
-    TextInputType keyboardtype) {
+    TextInputType keboardtype) {
   return TextFormField(
-    keyboardType: keyboardtype,
+    keyboardType: keboardtype,
     controller: textfieldcontroler,
     decoration: InputDecoration(
       labelText: placeholder,
