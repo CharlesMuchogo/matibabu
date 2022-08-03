68004a365032a73c943fecb3b67af8e52da276b0
CharlesMuchogo <muchpaul2@gmail.com>
2022-08-03 22:24:42 +0300

    Removed unused imports

--- lib/pages/doctor_information.dart
+++ lib/pages/doctor_information.dart
@@ -2,10 +2,9 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_launch/flutter_launch.dart';
+
 import 'package:matibabu/pages/bookingpage.dart';
 import 'package:url_launcher/url_launcher.dart';
-//import 'package:url_launcher/url_launcher.dart';
 
 class DoctorInfo extends StatelessWidget {
   final String doctorSpecialty;
